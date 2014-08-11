26a9af9d13f1d0cc425c0816e1c0af4e0e449767
John Sturdy <john.sturdy@arm.com>
2014-08-11 19:37:03 +0100

    convert keywords and tags

--- elisp/org-zorg.el
+++ elisp/org-zorg.el
@@ -5,39 +5,70 @@
 ;;; representing the number of them, keywords will be replaced by a
 ;;; short indication, with a keywords line in the file indicating the
 ;;; expansions, and the tags likewise.  The keywords line begins with
-;;; a vertical bar, and has space-separated keywords, with a vertical
-;;; bar (with a space on either side of it) marking the boundary at
-;;; which cycling a keyword should go back.  The tags line begins with
-;;; a colon, and has a colon before each tag, and none at the end.
+;;; an exclamation mark, and has space-separated keywords, with a
+;;; vertical bar (with a space on either side of it) marking the
+;;; division between not-done and done, and an exclamation mark (with
+;;; a space on either side of it) marking the boundary at which
+;;; cycling a keyword should go back.  The tags line begins with a
+;;; colon, and has a colon before each tag, and none at the end.
 
 (defun org-export-to-zorg (org-file zorg-file)
   "Convert ORG-FILE to ZORG-FILE."
   (interactive "fFile to export from:
 FFile to export into: ")
-  ;; todo: produce keywords line, shorten keywords
-  ;; todo: produce tags line, shorten tags
   (save-excursion
     (find-file zorg-file)
     (erase-buffer)
     (insert-file-contents org-file)
-    (goto-char (point-min))
-    (delete-non-matching-lines "^\\*")
-    (goto-char (point-min))
-    (while (not (eobp))
-      (cond
-       ((looking-at "^\\(\\*+\\)")
-	;; turn * sequences into numbers
-	;; todo: check they're not too large
-	;; todo: convert keys
-	;; todo: convert tags
-	(replace-match (int-to-string (- (match-end 1) (match-beginning 1))) nil t nil 1))
-       ((looking-at "^\\(\\s-+\\)")
-	;; start other lines with a single space
-	(replace-match " " nil t nil 1))
-       (t
-	(insert " "))
-       )
-      (beginning-of-line 2))
+    (org-mode)
+    (let ((file-tags (mapcar 'car (org-get-buffer-tags)))
+	  (file-keywords
+	   (apply 'append
+		  (cons '("!")
+			(mapcar (lambda (kl)
+				  (org-remove-keyword-keys (cdr kl)))
+				org-todo-keywords)))))
+      (goto-char (point-min))
+      (delete-non-matching-lines "^\\*")
+      (goto-char (point-min))
+      (while (not (eobp))
+	(cond
+	 ((looking-at "^\\(\\*+\\)")
+	  (let ((line-tags (save-match-data (org-get-tags))))
+	    (message "file-tags %S; line-tags %S" file-tags line-tags)
+	    ;; turn * sequences into numbers
+	    ;; todo: check they're not too large
+	    (replace-match (int-to-string (- (match-end 1) (match-beginning 1))) nil t nil 1)
+	    (beginning-of-line 1) (forward-char 2)
+	    (when (looking-at org-todo-regexp)
+	      (replace-match (format "!%d" (position (match-string-no-properties 1) file-keywords :test 'string=))))
+
+	    (when line-tags
+	      (insert " :"
+		      (mapconcat (lambda (tag)
+				   (int-to-string (position tag file-tags :test 'string=)))
+				 line-tags
+				 ":")
+		      " ")
+	      ;; todo: remove original tags
+	      )
+	    ))
+	 ((looking-at "^\\(\\s-+\\)")
+	  ;; start other lines with a single space
+	  (replace-match " " nil t nil 1))
+	 (t
+	  (insert " "))
+	 )
+	(beginning-of-line 2))
+      (goto-char (point-min))
+      (fundamental-mode)
+      (insert (mapconcat 'identity
+			 file-keywords
+			 " ")
+	      "\n")
+      (insert ":"
+	      (mapconcat 'identity file-tags ":")
+	      "\n"))
     (basic-save-buffer))
   )
 
