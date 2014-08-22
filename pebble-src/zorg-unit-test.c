e146071f6ff58a18c6dac4ca545e632df94cab83
John Sturdy <john.sturdy@arm.com>
2014-08-22 18:00:22 +0100

    Output which lines are expected in test output

--- pebble-src/zorg-unit-test.c
+++ pebble-src/zorg-unit-test.c
@@ -6,21 +6,21 @@
 #include "zorg.h"
 
 static char *test_data[] = {
-  "1 :1 First top-level item",
-  "2 :12 First second-level item within first top-level item",
-  "2 :3 Second second-level item within first top-level item",
-  "2 :8 Third second-level item within first top-level item",
-  "1 Second top-level item",
-  "2 First second-level item within second top-level item",
-  "2 Second second-level item within second top-level item",
-  "2 :8:12:9 Third second-level item within second top-level item", /* isn't working when :12: isn't the first tag */
-  "1 Third top-level item",
-  "2 First second-level item within third top-level item",
-  "3 First third-level item within first second-level item within third top-level item",
-  "3 :12 Second third-level item within first second-level item within third top-level item",
-  "2 Second second-level item within third top-level item",
-  "2 Third second-level item within third top-level item",
-  ":zero:one:two:three:four:five:six:seven:eight:nine:ten:eleven:twelve:thirteen:fourteen:fifteen:",
+  /* 0 */ "1 :1 First top-level item",
+  /* 1 */ "2 :12 First second-level item within first top-level item",
+  /* 2 */ "2 :3 Second second-level item within first top-level item",
+  /* 3 */ "2 :8 Third second-level item within first top-level item",
+  /* 4 */ "1 Second top-level item",
+  /* 5 */ "2 First second-level item within second top-level item",
+  /* 6 */ "2 Second second-level item within second top-level item",
+  /* 7 */ "2 :8:12:9 Third second-level item within second top-level item", /* isn't working when :12: isn't the first tag */
+  /* 8 */ "1 Third top-level item",
+  /* 9 */ "2 First second-level item within third top-level item",
+  /* 10 */ "3 First third-level item within first second-level item within third top-level item",
+  /* 11 */ "3 :12 Second third-level item within first second-level item within third top-level item",
+  /* 12 */ "2 Second second-level item within third top-level item",
+  /* 13 */ "2 Third second-level item within third top-level item",
+  /* 14 */ ":zero:one:two:three:four:five:six:seven:eight:nine:ten:eleven:twelve:thirteen:fourteen:fifteen:",
   NULL};
 
 #define SPECIAL_MAX 256
@@ -48,12 +48,12 @@ main(int argc, char **argv, char **env)
     add_line(*p);
   }
 
-  printf("Tree mode output:\n");
+  printf("Tree mode output (should be lines 0, 4, 8):\n");
   set_mode(tree);
   update_display_lines();
   scrollout_display_lines();
 
-  printf("Tags output:\n");
+  printf("Tags output: (should be lines 1, 7, 11)\n");
   strcpy(filter_search_string, "12");
   zorg_pebble_scan_tags();
   scrollout_display_lines();
