6f5372c591f92b809478412d2f5d38285f380151
John Sturdy <jcg.sturdy@gmail.com>
2014-09-06 00:15:19 +0100

    more sketchy bits of companion / server

--- companion/zorg-server.py
+++ companion/zorg-server.py
@@ -4,6 +4,8 @@
 
 # Companion app for zorg-mode, that will respond to requests from the watchapp
 
+import argparse
+import socket
 import PyOrgMode
 
 def serve_directory_start(directory):
@@ -30,9 +32,31 @@ def serve_file_next():
 def handle_keyword_change(change_string):
     pass
 
+def receive_from(client_socket):
+    chunks = []
+    while true:
+        chunk = client_socket.recv(1024)
+        if chunk == '':
+            return false
+        chunks.append(chunk)
+        if '\n' in chunk:
+            return ''.join(chunks)
+
 def start_zorg_server():
+    parser = argparse.ArgumentParser(description="Support a zorg Pebble app")
+    parser.add_argument('directory', help="The directory to scan for .org files")
+    args = parser.parse_args()
     # todo: open a socket, listen on it
-    pass
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # hostname = socket.gethostname()
+    hostname = 'localhost'
+    server_socket.bind((hostname, 8080))
+    server_socket.listen(5)
+    # normally will start with being asked to list the files in args.directory
+    while true:
+        (client_socket, address) = server_socket.accept()
+        # todo: put this into a thread of its own, and loop reading requests in it:
+        request = receive_from(client_socket)
 
 if __name__ == '__main__':
     startZorgServer()
