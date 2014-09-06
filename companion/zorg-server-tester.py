ae50f80b2889d5d7fa14f39a2095a4e89aee2555
John Sturdy <jcg.sturdy@gmail.com>
2014-09-06 21:43:04 +0100

    add little program for testing server

---
+++ companion/zorg-server-tester.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+A simple echo client, based on http://ilab.cs.byu.edu/python/socket/echoclient.html
+"""
+
+import socket
+
+host = 'localhost'
+port = 48080
+size = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host,port))
+s.send('D')
+data = s.recv(size)
+s.close()
+print 'Received:', data 
