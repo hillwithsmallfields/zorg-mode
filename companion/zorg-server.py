737765f27d6b9d748f35ffe2ba29e00e6cc696f1
John Sturdy <jcg.sturdy@gmail.com>
2014-09-09 22:27:33 +0100

    start using SocketServer drop PyOrgMode and read the lines myself

--- companion/zorg-server.py
+++ companion/zorg-server.py
@@ -3,16 +3,21 @@
 # uses https://github.com/bjonnh/PyOrgMode
 
 # Companion app for zorg-mode, that will respond to requests from the watchapp
+# using bits from https://docs.python.org/2/library/socketserver.html to get started
+
+# todo: stop using PyOrgMode and do it all myself, I think it'll be simpler for this application
 
 import argparse
-import socket
-import PyOrgMode
+import SocketServer
 import os
+import os.path
 import fnmatch
 
-port = 48088
-
+port = 48084
+org_directory = os.path.expanduser("~/Dropbox/org/")
+session_running = True
 directory_files = []
+file_lines = []
 
 def serve_directory_start(directory):
     """Return the first part of the listing of DIRECTORY."""
@@ -22,56 +27,68 @@ def serve_directory_start(directory):
     for file in os.listdir(directory):
         if fnmatch.fnmatch(file, "*.org"):
             directory_files.append(file)
-    print directory_files.pop(0)
+    return directory_files.pop(0)
 
 def serve_directory_next():
     """Return the next part of the listing of the current directory."""
     global directory_files
-    print directory_files.pop(0)
+    return directory_files.pop(0)
 
 def serve_file_start(filename):
     """Serve the first part of FILENAME."""
-    tree = PyOrgMode.OrgDataStructure()
-    print "about to load tree from", filename
-    tree.load_from_file(filename)
-    print "tree is", tree
-    # todo: get all keywords, tags, dates
-    # todo: count up the size, and send the size line
-    pass
+    global file_lines
+    filename = os.path.join(org_directory, filename)
+    file = open(filename)
+    file_lines = [ line.rstrip('\n') for line in file ]
+    file.close()
+    total_size = 0
+    for line in file_lines:
+        total_size += len(line) + 1
+    return "bytes=%d;lines=%d" % (total_size,len(file_lines))
 
 def serve_file_next():
     """Return the next part of the current file."""
     # todo: send the keywords, tags, and date lines on the first three calls, then start sending data lines
-    pass
+    global file_lines
+    return "file line to go here"
 
 def handle_keyword_change(change_string):
     pass
 
-def receive_from(socket):
-    chunks = []
-    while True:
-        chunk = socket.recv(1024)
-        if chunk == '':
-            return False
-        chunks.append(chunk)
-        if '\n' in chunk:
-            return ''.join(chunks)
-
-org_directory = "~/Dropbox/org/"
-
 def handle_request(request):
-    if request[0] == "f":
-        serve_file_next()
-    elif request[0] == "F":
-        serve_file_start(request[1:])
-    elif request[0] == "D":
-        serve_directory_start(org_directory)
-    elif request[0] == "d":
-        serve_directory_next()
+    global session_running
+    command = request[0]
+    if command == "f":
+        return serve_file_next()
+    elif command == "F":
+        return serve_file_start(request[1:])
+    elif command == "D":
+        return serve_directory_start(org_directory)
+    elif command == "d":
+        return serve_directory_next()
+    elif command == "Q":
+        session_running = False
+        return "quitting"
     pass
+
+class MyTCPHandler(SocketServer.StreamRequestHandler):
+
+    def handle(self):
+        self.data = self.rfile.readline().strip()
+        print "{} wrote:".format(self.client_address[0])
+        print self.data
+        result = handle_request(self.data) + "\n"
+        print "Writing back result", result
+        self.wfile.write(result)
 
 def start_zorg_server():
     global org_directory
+    global session_running
+    local = True
+    if local:
+        hostname = 'localhost'
+    else:
+        hostname = socket.gethostname()
     parser = argparse.ArgumentParser(description="Support a zorg Pebble app")
     parser.add_argument('directory', help="The directory to scan for .org files")
     args = parser.parse_args()
@@ -82,21 +99,13 @@ def start_zorg_server():
     for file in os.listdir(org_directory):
         print " * ", file
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    local = True
-    if local:
-        hostname = 'localhost'
-    else:
-        hostname = socket.gethostname()
-    server_socket.bind((hostname, port))
-    server_socket.listen(5)
-    while True:
-        client_socket, address = server_socket.accept()
-        # request = receive_from(client_socket)
-        request = client_socket.recv(1024)
-        if request:
-            handle_request(request)
-        client_socket.close()
+    print "Opening on port", port
+
+    server = SocketServer.TCPServer((hostname, port), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
 
 if __name__ == '__main__':
     start_zorg_server()
