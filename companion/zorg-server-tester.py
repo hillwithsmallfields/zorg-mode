3e615e9e6826c706575249e8561d9e5b063b32e3
John Sturdy <jcg.sturdy@gmail.com>
2014-09-09 22:27:45 +0100

    fiddling around

--- companion/zorg-server-tester.py
+++ companion/zorg-server-tester.py
@@ -6,12 +6,20 @@ A simple echo client, based on http://ilab.cs.byu.edu/python/socket/echoclient.h
 
 import socket
 
+commands = ['D\n',
+            'd\n',
+            'd\n',
+            'F/home/jcgs/Dropbox/org/general.org\n',
+            'Q\n']
+
 host = 'localhost'
-port = 48088
+port = 48080
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host,port))
-s.send('F/home/jcgs/Dropbox/org/general.org')
-data = s.recv(size)
-s.close()
-print 'Received:', data 
+for command in commands:
+    s.connect((host,port))
+    print 'Sending:', command
+    s.send(command)
+    data = s.recv(size)
+    print 'Received:', data 
+    s.close()
